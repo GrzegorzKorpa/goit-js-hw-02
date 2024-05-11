36e11f9516dcdf28b4430062052946e9b23fab3d
GrzegorzKorpa <grzegorz.korpa@post.pl>
2024-05-11 09:39:41 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,8 @@
 "use strict";
-
+function checkForSpam(message) {
+  const lowerCheckForSpam = checkForSpam.toLowerCase();
+  const hasSpam = lowerCheckForSpam.includes("SPAM");
+  const hasSale = lowerCheckForSpam.includes("SALE");
+  if(hasSpam = true || hasSale == true) {return true;}
+  else {return false};
+}
