98bc81c6102333fb75330eabbd5589d813a3fd19
GrzegorzKorpa <grzegorz.korpa@post.pl>
2024-05-11 10:15:28 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,16 @@
 "use strict";
 function checkForSpam(message) {
-  const lowerCheckForSpam = checkForSpam.toLowerCase();
-  const hasSpam = lowerCheckForSpam.includes("SPAM");
-  const hasSale = lowerCheckForSpam.includes("SALE");
-  if(hasSpam = true || hasSale == true) {return true;}
-  else {return false};
+const lowerCheckForSpam = checkForSpam.toLowerCase();
+const hasSpam = lowerCheckForSpam.includes("spam");
+const hasSale = lowerCheckForSpam.includes("sale");
+if (hasSpam === true || hasSale === true) { return true; }
+else {return false;}
 }
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
